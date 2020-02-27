577b538442450065d1d50fe86db0cd14193ab418
Drashti Kaushikkumar Pandya <dpandya@isimtl.local>
2020-02-27 14:17:30 -0500

    console excercise

---
+++ exercise-console_pandya-drashti/js/console.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const temperature = 21.3
+const fruits = ['apple', 'pear', 'orange']
+
+console.log("Yo.")
+console.log(temperature)
+console.log('The temprature is' + temperature)
+console.log('The collection of fruits is ', fruits)
+console.log("<h1>No HTML interpreter</h1>")
+console.log(18, "eighteen", true, null, undefined)
+console.log("This is a group", "\n", "Group content")
+console.log("This is a second group (closed by default)", "\n", "Content of the second group")
