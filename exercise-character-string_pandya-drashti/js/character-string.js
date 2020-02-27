fda45ebf335a2875c123177573a6a61e0b6decd1
Drashti Kaushikkumar Pandya <dpandya@isimtl.local>
2020-02-27 15:22:50 -0500

    character string done

---
+++ exercise-character-string_pandya-drashti/js/character-string.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const SENTENCE = 'The strength of JavaScript is that you can do anything. The weakness is that you will.'
+
+console.log('The whole sentence:', SENTENCE)
+
+console.log('The length of the string:', SENTENCE.length)
+
+console.log('The position of the word "JavaScript":', SENTENCE.indexOf('JavaScript'))
+
+console.log('The position of the last \'is\':', SENTENCE.lastIndexOf('is'))
+
+var parts = SENTENCE.split('.');
+console.log('The last sentence only:', SENTENCE[SENTENCE.length - 1])
+
+console.log('The 10 characters starting from the 17th character:', SENTENCE.substring(17, SENTENCE[SENTENCE.length - 1], 10))
+
+console.log('The number of words in the whole phrase:', str.split(' ').length)
+
+console.log('The 61st character:', SENTENCE.indexOf(61))
+
+console.log('Replace \'you\' with \'I\' in the whole phrase:', ? ? ? ? ? ? ? )
--- exercise-string-concatenation_pandya-drashti/js/string-concatenation.js
+++ exercise-string-concatenation_pandya-drashti/js/string-concatenation.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-const NAME = 'Reza';
-const AGE = 54;
-const YEAR = 2020;
-console.log(NAME + ': ' + AGE);
+const NAME = 'Reza'
+const AGE = 54
+const YEAR = 2020
+console.log(NAME + ': ' + AGE)
 
-console.log(NAME, AGE);
-console.log(NAME + AGE);
-console.log(YEAR + 54);
-console.log(NAME + AGE + YEAR);
-console.log(AGE + NAME + YEAR);
+console.log(NAME, AGE)
+console.log(NAME + AGE)
+console.log(YEAR + AGE)
+console.log(NAME + AGE + YEAR)
+console.log(AGE + NAME + YEAR)
